fbc18c136c1940fd7d902bd84171df126ea28926
servidor <giovani_cuiaba@hotmail.com>
2020-02-26 12:26:32 -0400

    06 Finalizando o fluxo do App

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/contact_form.dart';
 import 'package:bytebank/screens/contacts_list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
---
+++ lib/models/contact.dart
@@ -0,0 +1,11 @@
+class Contact {
+  final String name;
+  final int accountNuber;
+
+  Contact(this.name, this.accountNuber);
+
+  @override
+  String toString() {
+    return 'Contact{name: $name, accountNuber: $accountNuber}';
+  }
+}
